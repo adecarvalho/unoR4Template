af5dd6bac43bb93887888b6608ac076e47e9d9ae
adelino decarvalho <adelino@MacBook-Pro-de-adelino.local>
2024-06-06 10:11:40 +0200

    change README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # unoR4Template
+Projet sous Visual Studio Code pour la carte UnoR4-WiFi
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+// const
+
+// prototypes
+
+// obj
+
+// var global
+
+//************
+void setup()
+{
+  Serial.begin(115200);
+}
+
+//**************
+void loop()
+{
+  Serial.println("test UnoR4");
+  delay(500);
+}
